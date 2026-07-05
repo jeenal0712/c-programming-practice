cd39963e9d60df95e327ecd3e62475a818c71514
jeenal0712 <singhjeenal81@gmail.com>
2026-07-05 13:19:32 +0530

    Fix pair counting to avoid duplicates
    Start inner loop from i+1 instead of 0 to prevent counting the same pair twice and improve efficiency of the pair sum algorithm.

--- c-practice/Arrays/Arr_Problem4.c
+++ c-practice/Arrays/Arr_Problem4.c
@@ -4,7 +4,7 @@ int countsum(int arr[],int n,int x){
     int p=0;
     for (int i = 0; i <n; i++)
     {
-        for (int j = 0; j <n; j++)
+        for (int j = i+1; j <n; j++)
         {
             if (arr[i]+arr[j]==x)
             {
