f70086b775aa9dea2849bf9fd94a720a9c5e5c5c
jeenal0712 <singhjeenal81@gmail.com>
2026-07-14 15:38:01 +0530

    Add Bubble Sort implementation
    A C program that implements the bubble sort algorithm. It accepts user input for an array of elements, sorts them in ascending order, and displays the sorted result.

---
+++ c-practice/Sorting_Algorithms/Bubble_Sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter the total number of elements you want in array:");
+    scanf("%d",&n);
+
+    int a[n];
+    for (int i = 0; i <n; i++)
+    {
+        printf("enter the %d element of array:",i+1);
+        scanf("%d",&a[i]);
+    }
+    
+    for (int i = 0; i <=n-2; i++)
+    {
+        for (int j = 0; j <=n-i-2; j++)
+        {
+            if (a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    
+    printf("\nThe sorted array is:\n");
+    for (int i = 0; i <n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+    return 0;
+}
